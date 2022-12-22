59aa3d10731fb6ff70c2ba9108a8ff1afe94350d
deejay-oukay <dj-ok@mail.ru>
2022-12-22 18:52:41 +0500

    Устранена проблема с нулём в задаче 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -5,9 +5,15 @@
 }
 
 int N = InputInt("Введите число N: ");
-int increment = 1;
-if (N < 0)
-    increment = -1;
+if (N == 0)
+    //если N равно нулю, то цикл для него работать не будет
+    Console.WriteLine("Куб из 1 равен 1\nКуб из 0 равен 0");
+else
+{
+    int increment = 1;
+    if (N < 0)
+        increment = -1;
 
-for (int i = 1; Math.Abs(i) <= Math.Abs(N); i += increment)
-    Console.WriteLine($"Куб из {i} равен {Math.Pow(i,3)}");
+    for (int i = 1; Math.Abs(i) <= Math.Abs(N); i += increment)
+        Console.WriteLine($"Куб из {i} равен {Math.Pow(i,3)}");
+}
