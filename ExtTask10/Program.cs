62bd2f56ef11fdf3e81bc489072563b3687fd77c
deejay-oukay <dj-ok@mail.ru>
2022-12-23 18:00:12 +0500

    Ещё дополнительные задания. С наступающими праздниками! :)

---
+++ ExtTask10/Program.cs
@@ -0,0 +1,40 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool IsCorrectMonth(int number)
+{
+    if ((number > 0) && (number < 13))
+        return true;
+    else
+        return false;
+}
+
+bool IsCorrectDay(int day, int month)
+{
+    DateTime date = new DateTime();
+    if ((day > 0) && (day <= System.DateTime.DaysInMonth(date.Year, month)))
+        return true;
+    else
+        return false;
+}
+
+string[] months = {"Январь","Феваряль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+
+int monthNumber = 0;
+int dayNumber = 0;
+
+while (!IsCorrectMonth(monthNumber = InputInt("Укажите номер месяца: ")))
+    Console.Write("Нет такого месяца... ");
+
+DateTime date = new DateTime();
+Console.WriteLine($"Название месяца: {months[monthNumber-1]}\nКоличество дней в месяце: {System.DateTime.DaysInMonth(date.Year, monthNumber)}");
+
+while (!IsCorrectDay(dayNumber = InputInt("Укажите номер дня в месяце: "),monthNumber))
+    Console.Write("Нет такого дня в месяце... ");
+
+Console.WriteLine($"До конца месяца осталось дней: {System.DateTime.DaysInMonth(date.Year, monthNumber)-dayNumber}");
+
+Console.WriteLine("Следующий месяц: "+(monthNumber == 12 ? months[0]+". С наступающим! (-: так и появляются пасхалочки :-)" : months[monthNumber]));
---
+++ ExtTask6/Program.cs
@@ -0,0 +1,48 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool IsCorrect(int number)
+{
+    if (number > 1) //чтобы было, что сортировать, должно быть хотя бы 2 элемента в массиве
+        return true;
+    else
+        return false;
+}
+
+int Rand(int start, int finish)
+{
+    return new Random().Next(start,finish+1);
+}
+
+void PrintArray(int[] array)
+{
+    //?есть ли способ работать не с локальным, а с глобальным массивом при котором методы можно было бы описывать до основного кода?
+    Console.Write("Упорядоченные элементы массива: ");
+    string comma = "";
+
+    foreach (int item in array)
+    {
+        if ((item >= -20) && (item <= 20))
+        {
+            Console.Write($"{comma}{item}");
+            if (comma == "") //можно было бы присвоение делать при каждом проходе без условия, но частое чтение и однократная запись сработают быстрее, чем многократная запись без чтения
+                comma = ", ";
+        }
+    }
+}
+
+int n = 0;
+while (!IsCorrect(n = InputInt("Укажите число элементов для массива: ")))
+    Console.Write("Маловато будет... ");
+
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+    array[i] = Rand(-50, 50);
+
+Array.Sort(array);
+
+PrintArray(array);
