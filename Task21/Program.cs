80f1475ff21c607790a07decde1b409ac687eca3
deejay-oukay <dj-ok@mail.ru>
2022-12-23 11:08:13 +0500

    Последнее домашнее задание

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int
+    x1 = InputInt("Введите координату X для точки 1: "),
+    y1 = InputInt("Введите координату Y для точки 1: "),
+    z1 = InputInt("Введите координату Z для точки 1: "),
+    x2 = InputInt("Введите координату X для точки 2: "),
+    y2 = InputInt("Введите координату Y для точки 2: "),
+    z2 = InputInt("Введите координату Z для точки 2: ")
+;
+
+Console.WriteLine("Расстояние между двумя точками в 3D-пространстве = "+Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
