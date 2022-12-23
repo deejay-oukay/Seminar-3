6e3e45ca196c50d8b3a7163fb948311e8f189227
deejay-oukay <dj-ok@mail.ru>
2022-12-23 11:44:18 +0500

    Для вывода в задаче 17 создана функция

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -4,16 +4,21 @@
     return int.Parse(Console.ReadLine());
 }
 
+void Output(string message)
+{
+    Console.WriteLine(message);
+}
+
 int x = InputInt("Введите значение числа X: ");
 int y = InputInt("Введите значение числа Y: ");
 
 if ((x > 0) && (y > 0))
-    Console.WriteLine("1-ая четверть");
+    Output("1-ая четверть");
 else if ((x < 0) && (y > 0))
-    Console.WriteLine("2-ая четверть");
+    Output("2-ая четверть");
 else if ((x < 0) && (y < 0))
-    Console.WriteLine("3-ая четверть");
+    Output("3-ая четверть");
 else if ((x > 0) && (y < 0))
-    Console.WriteLine("4-ая четверть");
+    Output("4-ая четверть");
 else
-    Console.WriteLine("X и Y не должны быть равны 0");
+    Output("X и Y не должны быть равны 0");
